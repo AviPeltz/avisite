32907454acc567ca12f0ab2d238fe60702b1e84d
AviPeltz <aj.peltz@gmail.com>
2023-11-25 22:13:44 -0800

    Update navigation bar and add introductory message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,24 @@ export default function Home() {
   return (
     <>
       <div>
-      <h1 className="font-bold text-3xl">Hi! 👋 I'm Avi.</h1>
+        <nav className="bg-gray-800 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+              {/*<!-- Brand/Logo -->*/}
+              <a href="#" className="text-white text-lg font-bold">Avi Peltz</a>
+
+              {/*<!-- Navigation Links -->*/}
+              <div className="space-x-4">
+                <a href="#" className="text-white hover:text-gray-300">Home</a>
+                <a href="#" className="text-white hover:text-gray-300">Projects</a>
+                <a href="#" className="text-white hover:text-gray-300">Blog</a>
+                <a href="#" className="text-white hover:text-gray-300">Contact</a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      <div className="flex justify-center">
+        
+        <h1 className="p-2 font-bold text-3xl">Hi! 👋 I'm Avi.</h1>
       </div>
       <div className="flex justify-center items-center h-screen">
         <Button>Default Button</Button>
